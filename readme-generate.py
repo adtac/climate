e47c924ee340ccbcb13cc03bc043c74fed4b753a
Adhityaa C <c.adhityaa@gmail.com>
2016-11-29 20:41:10 +0530

    Change LICENSE to GPLv3
    Closes https://github.com/adtac/climate/issues/7

--- readme-generate.py
+++ readme-generate.py
@@ -92,9 +92,24 @@ for line in lines:
 print("""\n
 ## License
 
-Thanks to [guarinogabriel/Mac-CLI](https://github.com/guarinogabriel/Mac-CLI)
-for the idea.
+```
+    Climate - command line tools for Linux developers
+    Copyright (C) 2016  Adhityaa Chandrasekar
 
-See the [LICENSE](LICENSE.md) file for license rights and limitations (AGPL).
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+```
+
+See the [LICENSE](LICENSE) file for more details.
 
 """)
